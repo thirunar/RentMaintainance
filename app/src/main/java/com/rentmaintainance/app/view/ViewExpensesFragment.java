bbe5910c0053c76c3bf4e293049c95cc5ab57c99
Thirunavukkarasu <thirunar@thoughtworks.com>
2014-11-18 09:38:34 +0530

    Thiru | #24 | Fixed the issue in Expense and properties fragment.

--- app/src/main/java/com/rentmaintainance/app/view/ViewExpensesFragment.java
+++ app/src/main/java/com/rentmaintainance/app/view/ViewExpensesFragment.java
@@ -4,11 +4,7 @@ import android.app.Activity;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.DrawerLayout;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
+import android.view.*;
 import com.emilsjolander.components.StickyScrollViewItems.StickyScrollView;
 import com.rentmaintainance.app.Context;
 import com.rentmaintainance.app.R;
@@ -23,15 +19,14 @@ import it.gmariotti.cardslib.library.view.CardListView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ViewExpensesFragment extends android.app.Fragment {
+public class ViewExpensesFragment extends Fragment {
 
     private static final String TAG = "ViewExpensesFragment";
     private CardListView expenseListView;
-    private int defaultHeaderTopPos;
-    private int contentFrameLayoutPos;
-    private DrawerLayout drawerLayout;
     private float mActionBarHeight;
+    private BottomMenuFragment bottomMenuFragment;
 
+    private static View view;
 
     public ViewExpensesFragment(Activity activity) {
         Context.getInstance().updateApplicationContext(activity.getApplicationContext());
@@ -45,41 +40,78 @@ public class ViewExpensesFragment extends android.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_view_expenses, container, false);
-        initializeView(rootView);
 
-        return rootView;
+        if (view != null) {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null)
+                parent.removeView(view);
+        }
+        try {
+            view = inflater.inflate(R.layout.fragment_view_expenses, container, false);
+            initializeView(view);
+        } catch (InflateException e) {
+
+        }
+        return view;
     }
 
     private void initializeView(final View rootView) {
-        KenBurnsView headerPicture = (KenBurnsView) rootView.findViewById(R.id.header_picture);
-        headerPicture.setResourceIds(R.drawable.picture0, R.drawable.picture1);
+        setupKenBurnsView(rootView);
+        setupStickyScrollView(rootView);
+        setupExpenseListView(rootView);
+        setupBottomMenuFragment();
+    }
 
+    private void setupBottomMenuFragment() {
+        bottomMenuFragment = (BottomMenuFragment) getFragmentManager().findFragmentById(R.id.fragment_bottom_menu);
+        bottomMenuFragment.plusButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getActivity().getActionBar() != null) {
+                    getActivity().getActionBar().show();
+                }
+                android.support.v4.app.FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, new AddExpenseFragment(getActivity()))
+                        .commit();
+
+            }
+        });
+    }
+
+    private void setupExpenseListView(View rootView) {
+        expenseListView = (CardListView) rootView.findViewById(R.id.expenseListView);
+
+        List<Expense> expenses = Context.getInstance().allExpense().getAllExpense();
+        CardArrayAdapter expenseArrayAdapter = new CardArrayAdapter(getActivity(), getCardsFor(expenses));
+
+        expenseListView.setAdapter(expenseArrayAdapter);
+    }
+
+    private void setupStickyScrollView(final View rootView) {
         StickyScrollView stickyScroll = (StickyScrollView) rootView.findViewById(R.id.sticky_scroll);
-
         stickyScroll.setShadowHeight(50);
+
         final TypedArray styledAttributes = getActivity().getTheme().obtainStyledAttributes(
-                new int[] { android.R.attr.actionBarSize });
+                new int[]{android.R.attr.actionBarSize});
         mActionBarHeight = styledAttributes.getDimension(0, 0);
 
         stickyScroll.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
             @Override
             public void onScrollChanged() {
                 float y = rootView.findViewById(R.id.sticky_scroll).getScrollY();
-                if (y >= mActionBarHeight && getActivity().getActionBar().isShowing()) {
+                if (getActivity() != null && y >= mActionBarHeight && getActivity().getActionBar().isShowing()) {
                     getActivity().getActionBar().hide();
-                } else if ( y==0 && !getActivity().getActionBar().isShowing()) {
+                } else if (getActivity() != null && y == 0 && !getActivity().getActionBar().isShowing()) {
                     getActivity().getActionBar().show();
                 }
             }
         });
+    }
 
-        expenseListView = (CardListView) rootView.findViewById(R.id.expenseListView);
-
-        List<Expense> expenses = Context.getInstance().allExpense().getAllExpense();
-        CardArrayAdapter mCardArrayAdapter = new CardArrayAdapter(getActivity(), getCardsFor(expenses));
-
-        expenseListView.setAdapter(mCardArrayAdapter);
+    private void setupKenBurnsView(View rootView) {
+        KenBurnsView headerPicture = (KenBurnsView) rootView.findViewById(R.id.header_picture);
+        headerPicture.setResourceIds(R.drawable.picture0, R.drawable.picture1);
     }
 
     private List<Card> getCardsFor(List<Expense> expenses) {
@@ -101,4 +133,12 @@ public class ViewExpensesFragment extends android.app.Fragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
     }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (bottomMenuFragment != null)
+            getFragmentManager().beginTransaction().remove(bottomMenuFragment).commit();
+    }
+
 }
--- app/src/main/java/com/rentmaintainance/app/view/ViewPropertiesFragment.java
+++ app/src/main/java/com/rentmaintainance/app/view/ViewPropertiesFragment.java
@@ -1,30 +1,31 @@
 package com.rentmaintainance.app.view;
 
 import android.app.Activity;
-import android.app.FragmentManager;
+import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
+import android.support.v4.app.Fragment;
+import android.view.*;
 import android.widget.ListView;
 import com.emilsjolander.components.StickyScrollViewItems.StickyScrollView;
 import com.rentmaintainance.app.Context;
 import com.rentmaintainance.app.R;
 import com.rentmaintainance.app.customviews.KenBurnsView;
 import com.rentmaintainance.app.model.Property;
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
+import it.gmariotti.cardslib.library.internal.CardHeader;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ViewPropertiesFragment extends android.app.Fragment {
+public class ViewPropertiesFragment extends Fragment {
 
     private static final String TAG = "ViewPropertiesFragment";
     private ListView propertiesListView;
-    private int defaultHeaderTopPos;
-    private int contentFrameLayoutPos;
-    private DrawerLayout drawerLayout;
+    private float mActionBarHeight;
     private BottomMenuFragment bottomMenuFragment;
+
+    private static View view;
 
 
     public ViewPropertiesFragment(Activity activity) {
@@ -40,49 +41,85 @@ public class ViewPropertiesFragment extends android.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_view_properties, container, false);
 
-        initializeView(rootView);
+        if (view != null) {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null)
+                parent.removeView(view);
+        }
+        try {
+            view = inflater.inflate(R.layout.fragment_view_properties, container, false);
+            initializeView(view);
 
-        propertiesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        } catch (InflateException ignored) {
+        }
 
-                getFragmentManager().beginTransaction()
-                        .replace(R.id.container,
-                                ViewPropertyFragment.newInstance((Property) parent.getItemAtPosition(position)))
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
-        return rootView;
+        return view;
     }
 
-    private void initializeView(View rootView) {
-        KenBurnsView headerPicture = (KenBurnsView) rootView.findViewById(R.id.header_picture);
-        headerPicture.setResourceIds(R.drawable.picture0, R.drawable.picture1);
-
-        StickyScrollView stickyScroll = (StickyScrollView) rootView.findViewById(R.id.sticky_scroll);
-
-        stickyScroll.setShadowHeight(50);
-
-        propertiesListView = (ListView) rootView.findViewById(R.id.propertiesListView);
-
-        List<Property> properties = Context.getInstance().allProperties().getAllProperties();
-
-        PropertiesListAdaptor propertiesListAdaptor = new PropertiesListAdaptor(getActivity().getApplicationContext(), properties);
-        propertiesListView.setAdapter(propertiesListAdaptor);
+    private void initializeView(final View rootView) {
+        setupKenBurnsView(rootView);
+        setupStickyScrollView(rootView);
+        setupPropertiesListView(rootView);
 
         bottomMenuFragment = (BottomMenuFragment) getFragmentManager().findFragmentById(R.id.fragment_bottom_menu);
-
         bottomMenuFragment.plusButton().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentManager fragmentManager = getFragmentManager();
+                android.support.v4.app.FragmentManager fragmentManager = getFragmentManager();
                 fragmentManager.beginTransaction()
                         .replace(R.id.container, new AddPropertyFragment(getActivity()))
                         .commit();
+            }
+        });
+    }
 
+    private void setupPropertiesListView(View rootView) {
+        propertiesListView = (ListView) rootView.findViewById(R.id.propertiesListView);
+        List<Property> properties = Context.getInstance().allProperties().getAllProperties();
+        CardArrayAdapter propertiesArrayAdapter = new CardArrayAdapter(getActivity(), getCardsFor(properties));
+
+        propertiesListView.setAdapter(propertiesArrayAdapter);
+    }
+
+
+    private List<Card> getCardsFor(List<Property> properties) {
+        List<Card> cards = new ArrayList<Card>();
+        for (Property property : properties) {
+            CardHeader cardheader = new CardHeader(getActivity());
+            cardheader.setTitle(property.getHouseNumber());
+
+            Card card = new Card(getActivity().getApplicationContext());
+            card.setTitle(property.getName());
+            card.addCardHeader(cardheader);
+
+            cards.add(card);
+        }
+        return cards;
+    }
+
+    private void setupKenBurnsView(View rootView) {
+        KenBurnsView headerPicture = (KenBurnsView) rootView.findViewById(R.id.header_picture);
+        headerPicture.setResourceIds(R.drawable.picture0, R.drawable.picture1);
+    }
+
+    private void setupStickyScrollView(final View rootView) {
+        StickyScrollView stickyScroll = (StickyScrollView) rootView.findViewById(R.id.sticky_scroll);
+        stickyScroll.setShadowHeight(50);
+
+        final TypedArray styledAttributes = getActivity().getTheme().obtainStyledAttributes(
+                new int[]{android.R.attr.actionBarSize});
+        mActionBarHeight = styledAttributes.getDimension(0, 0);
+
+        stickyScroll.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                float y = rootView.findViewById(R.id.sticky_scroll).getScrollY();
+                if (getActivity() != null && y >= mActionBarHeight && getActivity().getActionBar().isShowing()) {
+                    getActivity().getActionBar().hide();
+                } else if (getActivity() != null && y == 0 && !getActivity().getActionBar().isShowing()) {
+                    getActivity().getActionBar().show();
+                }
             }
         });
     }
@@ -91,4 +128,12 @@ public class ViewPropertiesFragment extends android.app.Fragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
     }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (bottomMenuFragment != null)
+            getFragmentManager().beginTransaction().remove(bottomMenuFragment).commit();
+    }
+
 }
